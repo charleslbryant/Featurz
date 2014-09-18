015cdcd98428b7cf99b136725b7bb7aae8434e56
Bryant, Charles <newjaxcom@gmail.com>
2014-09-18 14:37:55 -0400

    Cleaning up and adding UserListItemVm.cs.

---
+++ source/application/Web/ViewModel/User/UserListItemVm.cs
@@ -0,0 +1,30 @@
+﻿namespace Featurz.Web.ViewModel.User
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class UserListItemVm
+	{
+		public string Email { get; set; }
+
+		public string FirstName { get; set; }
+
+		public ICollection<string> Groups { get; set; }
+
+		public string Id { get; set; }
+
+		public bool IsEnabled { get; set; }
+
+		public string LastName { get; set; }
+
+		public string Name
+		{
+			get
+			{
+				return this.FirstName + " " + this.LastName;
+			}
+		}
+
+		public ICollection<string> Roles { get; set; }
+	}
+}
